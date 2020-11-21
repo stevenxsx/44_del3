3f110413a34bbaf178b9ed41f2b154c27562f0e0
Schnack <simon.schnack99@gmail.com>
2020-11-21 20:21:55 +0100

    Har lavet første ChanceCard, Men har de andre nødvendige klasser mangler, så kaldemetoderne er sikkert noget sludder.

--- src/ChanceCards.java
+++ src/ChanceCards.java
@@ -18,8 +18,21 @@ public class ChanceCards extends ChanceCardController {
     //Herfra skal jeg "bare" lave koden for hvad hvert Chancekort skal gøre, ud fra en switch
     //så nu man lander på ChanceCardControllerne, så vælger den et tilfældigt af de 20 eksistrende
     //chancekort som der er.
+
     public void cardPile () {
         switch (cardNumber) {
+            case 1:
+                /*Ryk frem til start
+                player.setPosition(1);
+                player.addCash(PlayerAccount: 2)
+                Jeg har hverken Player eller Account klassen til rådighed, så jeg gætter på hvad
+                kaldemetoden ender med; Skal nok rettes senere.
+                (--------------)
+                Her skal der nok indsættes en gui-funktion der fortæller spilleren hvad der sker.
+                Har ikke en Gui-klasse, så gætter igen...
+                */
+                break;
+        }
 
         }
     }
