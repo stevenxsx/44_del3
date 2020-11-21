57069e46b84c753ecfdc94570f64f5c9b05a94fe
Schnack <simon.schnack99@gmail.com>
2020-11-21 20:00:27 +0100

    nu hvis man lander på ChanceCardControllerne, så vælger den et tilfældigt af de 20 eksistrende chancekort som der er, hvis det her virker.

--- src/ChanceCards.java
+++ src/ChanceCards.java
@@ -1,3 +1,25 @@
-public class ChanceCards extends ChanceCardField {
-//her skal alle chancekort være ....ChanceCardController
-}
+public class ChanceCards extends ChanceCardController {
+
+    int cardNumber;
+    Player player;
+    //GUI gui;
+    int turn;
+
+    int total=0;
+
+
+    public ChanceCards(int cardNumber, Player player, /*GUI gui,*/ int i) {
+        this.cardNumber=cardNumber;
+        this.player=player;
+        //this.gui=gui;
+        this.turn=i;
+
+    }
+    //Herfra skal jeg "bare" lave koden for hvad hvert Chancekort skal gøre, ud fra en switch
+    //så nu man lander på ChanceCardControllerne, så vælger den et tilfældigt af de 20 eksistrende
+    //chancekort som der er.
+    public void cardPile () {
+        switch (cardNumber) {
+
+        }
+    }
