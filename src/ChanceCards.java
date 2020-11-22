bf2a8225d6e38531d62a87f5f714b1b28774bc24
Schnack <simon.schnack99@gmail.com>
2020-11-22 01:01:59 +0100

    changed something

--- src/ChanceCards.java
+++ src/ChanceCards.java
@@ -101,6 +101,6 @@ public class ChanceCards extends ChanceCardController {
         }
     }
     /*Standartcommit
-    Har lavet ChanceCard nr. 2, Men de andre nødvendige klasser
+Changes     Har lavet ChanceCard nr. ??, Men de andre nødvendige klasser
 mangler, så kaldemetoderne er sikkert noget sludder.
      */
