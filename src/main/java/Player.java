4b9eacba979c6bb72f0867f7314f74b04593b25e
Katrine <KatsCoding@users.noreply.github.com>
2020-11-22 01:23:21 +0100

    made changes to player to include player positions for fields to use

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -22,4 +22,45 @@ public class Player {
     public int getCoins() {
         return account.getCoins();
     }
+
+    // SIMON
+
+    //Vi skal have en position og en boolean for om man er i fængsel ;)
+    private int playerPosition;
+
+    public void setPlayerPosition(int position){
+        if (position < 0) {
+            playerPosition = 0;
+        } else if (position > 23) {
+            playerPosition = 0;
+        }
+        else
+            playerPosition = position;
+    }
+
+    /**
+     * method used to move player positions, as well as making sure it continues to move after one round.
+     * @param amount number of fields to move
+     */
+    public void movePlayerPosition(int amount){
+        if (playerPosition + amount > 23) { // Making sure players can go in circles.
+            amount = playerPosition + amount - 23;
+            setPlayerPosition(0);
+        }
+        setPlayerPosition(playerPosition + amount);
+    }
+
+    public int getPlayerPosition() {
+        return playerPosition;
+    }
+
+    private boolean isJailed;
+
+    public boolean isJailed() {
+        return isJailed;
+    }
+
+    public void setJailed(boolean jailed) {
+        isJailed = jailed;
+    }
 }
