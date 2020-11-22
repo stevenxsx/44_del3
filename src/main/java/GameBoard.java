2375148cca44889f59b66266088474d58f98984e
Schnack <simon.schnack99@gmail.com>
2020-11-22 03:24:04 +0100

    some new shit

--- src/main/java/GameBoard.java
+++ src/main/java/GameBoard.java
@@ -4,36 +4,29 @@ public class GameBoard {
 
     //et array som indeholder alle felter og deres attributter
     public GameBoard() {
-        gameBoardList[1] = new FieldStart();
-        gameBoardList[2] = new FieldStreet("M1","Burgerbaren",false,'a', 1,1 );
-        gameBoardList[3] = new FieldStreet("M1", "Pizzariaet",false,'a',1,1);
-        gameBoardList[4] = new ChanceCardController();
-        gameBoardList[5] = new FieldStreet("M1","Slikbutikken",false,'b',1,1);
-        gameBoardList[6] = new FieldStreet("M1","Iskiosken",false,'b',1,1);
-        gameBoardList[7] = new visitJail("På besøg");
-        gameBoardList[8] = new FieldStreet("M2","Museet",false,'c',2,1);
-        gameBoardList[9] = new FieldStreet("M2","Bibliotek",false,'c',2,1);
-        gameBoardList[10] = new ChanceCardController();
-        gameBoardList[11] = new FieldStreet("M2","Skaterparken",false,'d',2,1);
-        gameBoardList[12] = new FieldStreet("M2","svømmepoolen",false,'d',2,1);
-        gameBoardList[13] = new FieldParking("Gratis parkering");
-        gameBoardList[14] = new FieldStreet("M3","Spillehallen",false,'e',3,2);
-        gameBoardList[15] = new FieldStreet("M3","Biograffen",false,'e',3,2);
-        gameBoardList[16] = new ChanceCardController();
-        gameBoardList[17] = new FieldStreet("M3","Legetøjsbutikken",false,'f',3,2);
-        gameBoardList[18] = new FieldStreet("M3","Dyrehandlen",false,'f',3,2);
-        gameBoardList[19] = new FieldJail();
-        gameBoardList[20] = new FieldStreet("M4","Bowlinghallen",false,'g',4,2);
-        gameBoardList[21] = new FieldStreet("M4","Zoo",false,'g',4,2);
-        gameBoardList[22] = new ChanceCardController();
-        gameBoardList[23] = new FieldStreet("M5","Vandlandet",false,'h',5,3);
-        gameBoardList[24] = new FieldStreet("M5","Strandpromaden",false,'h',5,3);
-
-
-
+        gameBoardList[0] = new FieldStart();
+        gameBoardList[1] = new FieldStreet("M1","Burgerbaren",false,'a', 1,1 );
+        gameBoardList[2] = new FieldStreet("M1", "Pizzariaet",false,'a',1,1);
+        gameBoardList[3] = new ChanceCardController();
+        gameBoardList[4] = new FieldStreet("M1","Slikbutikken",false,'b',1,1);
+        gameBoardList[5] = new FieldStreet("M1","Iskiosken",false,'b',1,1);
+        gameBoardList[6] = new visitJail("På besøg");
+        gameBoardList[7] = new FieldStreet("M2","Museet",false,'c',2,1);
+        gameBoardList[8] = new FieldStreet("M2","Bibliotek",false,'c',2,1);
+        gameBoardList[9] = new ChanceCardController();
+        gameBoardList[10] = new FieldStreet("M2","Skaterparken",false,'d',2,1);
+        gameBoardList[11] = new FieldStreet("M2","svømmepoolen",false,'d',2,1);
+        gameBoardList[12] = new FieldParking("Gratis parkering");
+        gameBoardList[13] = new FieldStreet("M3","Spillehallen",false,'e',3,2);
+        gameBoardList[14] = new FieldStreet("M3","Biograffen",false,'e',3,2);
+        gameBoardList[15] = new ChanceCardController();
+        gameBoardList[16] = new FieldStreet("M3","Legetøjsbutikken",false,'f',3,2);
+        gameBoardList[17] = new FieldStreet("M3","Dyrehandlen",false,'f',3,2);
+        gameBoardList[18] = new FieldJail();
+        gameBoardList[19] = new FieldStreet("M4","Bowlinghallen",false,'g',4,2);
+        gameBoardList[20] = new FieldStreet("M4","Zoo",false,'g',4,2);
+        gameBoardList[21] = new ChanceCardController();
+        gameBoardList[22] = new FieldStreet("M5","Vandlandet",false,'h',5,3);
+        gameBoardList[23] = new FieldStreet("M5","Strandpromaden",false,'h',5,3);
         }
-
-
-
-
     }
