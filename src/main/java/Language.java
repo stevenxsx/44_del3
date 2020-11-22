b8157b5bf23d79523315d1dc0273494ecaadd4d8
olinemelindaandersen <oline_andersen@hotmail.com>
2020-11-22 18:45:36 +0100

    Language branch and class made

---
+++ src/main/java/Language.java
@@ -0,0 +1,49 @@
+public class Language {
+
+
+    //Add indexes to the arrays with translated text.
+    //Index 0 = Danish, Index 1 = English, Index 2 = German, etc.
+    public String language = "Index 0= Danish, Index 1 = English, Index 2 = German,";
+
+    //Strings for welcomeMessage()
+    public String[] welcomeMessage = {"Velkommen til Monopoly Junior"};
+
+    // String for makeFields()
+    public String[] fieldDescriptionChance = {"Tag det øverste chancekort, læs det højt, og følg instruktionerne.\n"+
+    "Læg dit brugte kort tilbage nederst i bunken."};
+    public String[] fieldDescriptionStart ={""};
+    public String[] fieldDescriptionPåBesøg = {"dd"};
+    public String[] fieldDescriptionGåiFængsel = {""};
+    public String[] fieldDescriptionGratisParkering = {"Du behøver ikke gøre noget, bare snup dig en pause"};
+
+    // Strings for ChanceCards ()
+    public String[] chanceCard1 = {"Giv dette kort til BILEN, og tag et chancekort mere.\n"+
+    "BIL: På din næste tur skal du drøne frem til et hvilket som helst ledigt felt og købe det. \n"+
+    "Hvis der ikke er nogen ledige felter, skal du købe et fra en anden spiller"};
+    public String[] chanceCard2 = {"Ryk frem til START. Modtag "};....
+    public String[] chanceCard3 = {"Ryk op til 5 felter frem"};
+    public String[] chanceCard4 = {"GRATIS FELT!\n"+"Ryk frem til et orange felt. Hvis det er ledigt får du det GRATIS!\n"+
+    "Ellers skal du BETALE leje til ejeren "};
+    public String[] chanceCard5 = {"Ryk 1 felt frem, eller tag et chancekort mere."};
+    public String[] chanceCard6 = {"Giv dette kort til SKIBET, og tag et chancekort.\n"+
+    "SKIB: På din næste tur skal du sejle frem til et hvilket som helst ledigt felt og købe det.\n"+
+    "Hvis der ikke er nogen ledige felter, skal du købe et fra en anden spiller!"};
+    public String[] chanceCard7 = {"Du har spist for meget slik. BETAL M2 til banken"};
+    public String[] chanceCard8 = {"GRATIS FELT!\n"+ "Ryk frem til et orange eller grønt felt.\n"+
+    "Hvis det er ledigt får du det GRATIS! Ellers skal du betale leje til ejeren."};
+    public String[] chanceCard9 = {"GRATIS FELT!\n"+ "Ryk frem til et lyseblåt felt. Hvis det er ledigt får du det GRATIS \n"+
+    "Ellers skal du BETALE leje til ejeren"};
+    public String[] chanceCard10 = {"Du løslades uden omkostninger\n"+ "Behold dette kort, indtil du får brug for det"};
+    public String[] chanceCard11 = {"Ryk frem til Strandpromonaden"};
+    public String[] chanceCard12 = {"Giv dette kort til KATTEN, og tag et chancekort mere. \n"+
+    "KAT: På din næste tur skal du liste dig hen på et hvilket som helst ledigt felt og købe det.\n"+
+    "Hvis der ikke er nogen ledige felter, skal du købe et fra en anden spiller!"};
+    public String[] chanceCard13 = {"Giv dette kort til HUNDEN, og tag et chancekort mere.\n"+
+    "HUND: På din næste tur skal du hoppe hen på et hvilket som helst ledigt felt og købe det.\n"+
+    "Hvis der ikke er nogen ledige felter, skal du købe et fra en anden spiller!"};
+    
+
+
+
+}
+
