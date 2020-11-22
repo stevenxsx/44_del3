234034b658259f18dda34c9c2f75257ab7bb9da9
steveng85 <71653345+steveng85@users.noreply.github.com>
2020-11-22 13:30:08 +0100

    made two methods one for adding players and "cars", that is flexible for different choice for how many people need to join and make GUI.

--- src/GUI_Controller.java
+++ src/GUI_Controller.java
@@ -1,7 +1,30 @@
 
 
+import CDIO2_Game.FieldFactory;
+import gui_fields.GUI_Car;
+import gui_fields.GUI_Field;
+import gui_fields.GUI_Player;
+import gui_main.GUI;
+
 import java.awt.*;
 
 public class GUI_Controller {
+    private GUI gui;
+    private GUI_Player[] player;
+    private GUI_Car[] car;
 
+    public void makeGUI(){
+        GUI_Field[] fields = GameBoard();
+
+        gui =new GUI(fields, Color.PINK);
+    }
+    public void antalSpillere(){
+        player = new GUI_Player[t.length()];
+        car = new GUI_Car[t.length];
+        for (int i = 0; i < t.length; i++) {
+            cars[i] = new GUI_Car(Color.RED, Color.cyan, GUI_Car.Type.CAR, GUI_Car.Pattern.CHECKERED);
+            player[i] = new GUI_Player(t[i].getName(),p[i].setBlance(), car[i]);
+            gui.addPlayer(player[i])
+        }
+    }
 }
