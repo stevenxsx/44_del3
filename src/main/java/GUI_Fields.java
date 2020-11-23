b9a5ac2efb9ccbeb73d4429616cdae50b36b6b1e
steveng85 <71653345+steveng85@users.noreply.github.com>
2020-11-23 17:41:55 +0100

    GUI prototype is done

--- src/main/java/GUI_Fields.java
+++ src/main/java/GUI_Fields.java
@@ -6,13 +6,13 @@ public class GUI_Fields {
     public static GUI_Field[] makeGUIFields() {
         GUI_Field[] fields = new GUI_Field[24];
         int i = 0;
-        fields[i++] = new GUI_Start("Start", "Modtag M2 fra Banken, hver gang passerer eller lander på START", "", Color.WHITE, Color.BLACK);
+        fields[i++] = new GUI_Start("Start", "Modtag M2", "Modtag M2 fra Banken, hver gang passerer eller lander på START", Color.WHITE, Color.BLACK);
         fields[i++] = new GUI_Street("BURGER-\nBAREN", "", "", "1M", Color.WHITE, Color.BLACK);
         fields[i++] = new GUI_Street("PIZZA-\nHUSET", "", "", "1M", Color.WHITE, Color.BLACK);
         fields[i++] = new GUI_Chance("?", "Chance", "", Color.WHITE, Color.BLACK);
-        fields[i++] = new GUI_Street("SLIKBUTIKKEN", "", "", "1M", Color.WHITE, Color.BLACK);
+        fields[i++] = new GUI_Street("SLIK-\nBUTIKKEN", "", "", "1M", Color.WHITE, Color.BLACK);
         fields[i++] = new GUI_Street("ISKIOSKEN", "", "", "1M", Color.WHITE, Color.BLACK);
-        fields[i++] = new GUI_Jail("default", "gå I FÆNGSEL", "PÅ BESØG", "", Color.WHITE, Color.BLACK);
+        fields[i++] = new GUI_Jail("default", "", "PÅ BESØG", "", Color.WHITE, Color.BLACK);
         fields[i++] = new GUI_Street("MUSEET", "", "", "2M", Color.WHITE, Color.BLACK);
         fields[i++] = new GUI_Street("BIBLIOTEKT", "", "", "2M", Color.WHITE, Color.BLACK);
         fields[i++] = new GUI_Chance("?", "Chance", "", Color.WHITE, Color.BLACK);
@@ -21,13 +21,13 @@ public class GUI_Fields {
         fields[i++] = new GUI_Refuge("default", "GRATIS", "PARKERING", "", Color.WHITE, Color.BLUE);
         fields[i++] = new GUI_Street("SPILLE-\nHALLEN", "", "", "3M", Color.WHITE, Color.BLACK);
         fields[i++] = new GUI_Street("KINOEN", "", "", "3M", Color.WHITE, Color.BLACK);
-        fields[i++] = new GUI_Chance("?", "", "", Color.WHITE, Color.BLACK);
+        fields[i++] = new GUI_Chance("?", "CHANGE", "", Color.WHITE, Color.BLACK);
         fields[i++] = new GUI_Street("LEGETØJS-\nBUTIKKEN", "", "", "3M", Color.WHITE, Color.BLACK);
         fields[i++] = new GUI_Street("DYREHAND-\nLEN", "", "", "3M", Color.WHITE, Color.BLACK);
-        fields[i++] = new GUI_Jail("default", "I FÆNGSEL", "PÅ BESØG", "", Color.WHITE, Color.BLACK);
+        fields[i++] = new GUI_Jail("default", "", "FÆNGSLET", "", Color.WHITE, Color.BLACK);
         fields[i++] = new GUI_Street("BOWLING-\nHALLEN", "", "", "4M", Color.WHITE, Color.BLACK);
         fields[i++] = new GUI_Street("ZOO", "", "", "4M", Color.WHITE, Color.BLACK);
-        fields[i++] = new GUI_Chance("?", "", "", Color.WHITE, Color.BLACK);
+        fields[i++] = new GUI_Chance("?", "CHANCE", "", Color.WHITE, Color.BLACK);
         fields[i++] = new GUI_Street("VAND-\nLANDET", "", "", "5M", Color.WHITE, Color.BLACK);
         fields[i] = new GUI_Street("STRAND-\nPROMENADEN", "", "", "5M", Color.WHITE, Color.BLACK);
         return fields;
