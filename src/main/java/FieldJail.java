6ce1fbaa8a796d82792ccce3baaeb6774a6435cc
Schnack <simon.schnack99@gmail.com>
2020-11-24 15:28:50 +0100

    Katrin, have made the boolean calling if a player is in jail-ish

--- src/main/java/FieldJail.java
+++ src/main/java/FieldJail.java
@@ -1,20 +1,15 @@
 public class FieldJail extends Field {
-    /*
-    // har må der komme en spiller klasse ind, når jeg kender den.
     Player player;
-    //private int timer;
+    private int timer;
 
     public FieldJail(){}
 
-    //sender spilleren til fængslet
+    //sender spilleren til fængslet, og lavet et klad der kan bruges i Game klassen.
     public void goToJail(Player player){
         this.player=player;
-        //player.setPosition(??);
-        // Ideen er der skal være en opkrævning til næste tur, men det må ligge i spiller.... tror jeg
-        // Det tror jeg er en god ide - Jeg har lavet et udkast til det i player, helt nederst, det er kommenteret med SIMON foran, vær sød at se om det kan bruges - Kat
-
+        player.setPlayerPosition(10);
+        this.timer=0;
         player.setJailed(true);
-
     }
-*/
+
 }
