59f555e34e3472b5d917145d2c2a760e513c5ac5
Schnack <simon.schnack99@gmail.com>
2020-11-25 19:55:03 +0100

    minor fix

--- src/main/java/ChanceCardController.java
+++ src/main/java/ChanceCardController.java
@@ -1,9 +1,12 @@
 import java.util.Random;
+import gui_fields.GUI_Player;
+import gui_main.GUI;
+import gui_fields.*;
 
 public class ChanceCardController extends Field {
 
     //her vil jeg prøve at lave det så at classen henter kortene fra bunken (ChanceCard)
-    /*int i;
+    int i;
 
 
 
@@ -12,16 +15,16 @@ public class ChanceCardController extends Field {
     }
 
     //trækker et kort fra bunken af chancekort
-    public int getCard (Player player, int i/*Her skal den hente" GUI gui" og spilleren der rammer  *//*) {
+    public int getCard (Player player, GUI gui, int i, int arrayLength, Player[] playerArray) {
         this.i =i;
         Random cardPile = new Random();
         int card = cardPile.nextInt(20) + 1;
 // Nu er det sat op så man kan en af de 20 tilfældige chance-kort når jeg har lavet dem 
-        ChanceCards chanceCard = new ChanceCards(card, player, gui, i);
+        ChanceCards chanceCard = new ChanceCards(card, player, gui, i, playerArray, arrayLength);
         chanceCard.cardPile();
         i= chanceCard.getTurn();
         return i;
-    }*/
+    }
 }
 
 /* Jeg er lidt forvirret over hvorfor der både er en Chancecards og en ChanceCardsController, idet man normalt ikke laver en controller som der
